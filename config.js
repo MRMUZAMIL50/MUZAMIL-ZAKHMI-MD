3318999c6dbdccf3d3d12d9010c1f63ca362c22e
ishan96 <114572160+Ishan-md96@users.noreply.github.com>
2025-01-13 07:42:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ZxpjCYzK#bOtggFBaIGjDzC6pJAadP2ZeFVzUGjUf4IjeKWpqwHY",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -15,9 +15,9 @@ AUTO_STATUS_MSG: process.env.AUTO_STATUS__MSG || "*Jᴜsᴛ Nᴏᴡ Sᴛᴀᴛ�
 // set the auto reply massage on status reply  
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "MUZAMIL-ZAKHMI-MD",
+BOT_NAME: process.env.BOT_NAME || "ISHAN-MD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "MUZAMIL-ZAKHMI-MD",
+STICKER_NAME: process.env.STICKER_NAME || "ISHAN-MD",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -25,15 +25,15 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "92342758XXXX",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94768562916",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Mʀ Mᴜᴢᴀᴍɪʟ",
+OWNER_NAME: process.env.OWNER_NAME || "Mʀ Ishan",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ Mᴜᴢᴀᴍɪʟ*",
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ Ishan*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/ZJwzVrG/temp-image.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *MUZAMIL-ZAKHMI*🤠",
+LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar ISHAN MD*🤠",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
